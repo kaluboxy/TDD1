5b9f34e1e078dacfb9bc292d2e9c6fd6309745de
Kalub St. Clair <kaluboxy@hotmail.com>
2024-10-17 14:39:28 -0400

    First cut of a Dockerfile

---
+++ src/superlists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+from lists import views as list_views  
+
+urlpatterns = [
+    path("", list_views.home_page, name="home"),
+    path("lists/", include("lists.urls")),  
+]
